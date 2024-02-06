dd6f96aef9f63a2ea6f3fa238d604978e579ba1d
dyadyasawa <dyadyasawa@gmail.com>
2024-02-07 00:00:06 +0300

    Рабочие моменты.

--- classes.py
+++ classes.py
@@ -1,16 +1,17 @@
 
 import requests
-from pprint import pprint
 import json
 
 class HeadHunterApi:
     """ Класс для работы с данными через API сайта hh.ru. """
 
+    def __init__(self, key_word):
+        self.key_word = key_word
     def get_employers(self):
         """ Метод для подключения к API и получения данных с hh.ru. """
 
         url = 'http://api.hh.ru/employers'
-        params = {'text': 'высотные работы', 'area': 113}
+        params = {'text': {self.key_word}, 'area': 113}
 
         dict_info = requests.get(url, params=params).json()
 
@@ -21,7 +22,3 @@ class HeadHunterApi:
 
         with open('data/employers.json', 'w', encoding=('utf-8')) as f:
             json.dump(json_file, f, ensure_ascii=False, indent=4)
-
-
-hh = HeadHunterApi()
-pprint(hh.write_to_json(hh.get_employers()))
